d9eb2fbb973933e23ed36911f3853c83415f7938
NaufalZulfa <90429395+NaufalZulfa@users.noreply.github.com>
2021-09-30 12:11:25 +0700

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -1,9 +1,6 @@
-public class Helloworld{
+public class HelloWorld{
 	
 	public static void main(String args[]){
-		System.out.println("Nama : Naufal Zulfa Irawan");
-		System.out.println("Nim : 6706213087");
-		System.out.println("Kelas : RPLA 45-04");
-
+		System.out.println("HelloWorld");
 	}
 }
