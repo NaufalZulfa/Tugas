070cd91a179f3f282131894f1115b3052cd46490
NaufalZulfa <naufalzulfa212@gmail.com>
2021-09-30 12:07:43 +0700

    first commit

---
+++ Datadiri.java
@@ -0,0 +1,9 @@
+public class Datadiri{
+	
+	public static void main(String args[]){
+		System.out.println("Nama : Naufal Zulfa Irawan");
+		System.out.println("Nim : 6706213087");
+		System.out.println("Kelas : RPLA 45-04");
+
+	}
+}
